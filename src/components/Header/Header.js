879bfe2b66b7b200fdbb2f0070855c0307d42de5
Jack Gay <jackpowis@gmail.com>
2019-05-18 13:05:47 -0400

    working on jack header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import "./Header.css";
+import Logo from "../../Assets/Icons/HEADER/LOGO.png";
+import NavProfile from "../../Assets/Icons/HEADER/NAV_PROFILE.png";
+import NavStar from "../../Assets/Icons/HEADER/NAV_STAR.png";
+import NavSearch from "../../Assets/Icons/HEADER/NAV_SEARCH.png";
+import NavLocation from "../../Assets/Icons/HEADER/NAV_LOCATION.png";
 
 class Header extends Component {
     render () {
@@ -6,19 +12,24 @@ class Header extends Component {
             <div>
                 <header>
 
-            <div>
-                <h1><a href="">MADE<h1 className="" >2</h1>FIT</a></h1>                
+            <div className="header-top">
+                <img src={NavProfile} id="profile-logo"></img>
+                <img src={NavStar} id="star-logo"></img>    
+                <img src={Logo} id="head-logo" ></img>
+                <img src={NavSearch} id="search-logo"></img>
+                <img src={NavLocation} id="location-logo"></img>
+                {/* <img src={Logo}></img>                                 */}
             </div>
         </header>
         <nav>            
-            <ul>
-                <li><a href="#">GET STARTED</a></li>
-                <li><a href="#">PANTS</a></li>
-                <li><a href="#">TOPS</a></li>
-                <li><a href="#">OUTERWEAR</a></li>
-                <li><a href="#">SHOES</a></li>
-                <li><a href="#">ACCESORIES</a></li>
-                <li><a href="#">SALE</a></li>
+            <ul className="header-nav">
+                <li>GET STARTED</li>
+                <li>PANTS</li>
+                <li>TOPS</li>
+                <li>OUTERWEAR</li>
+                <li>SHOES</li>
+                <li>ACCESORIES</li>
+                <li>SALE</li>
             </ul>
         </nav>
         </div>
