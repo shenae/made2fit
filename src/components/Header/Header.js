0cb0e00403201077b2985a54629db86bcc580e0d
Jack Gay <jackpowis@gmail.com>
2019-05-18 13:25:30 -0400

    commit: final header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,33 +7,36 @@ import NavSearch from "../../Assets/Icons/HEADER/NAV_SEARCH.png";
 import NavLocation from "../../Assets/Icons/HEADER/NAV_LOCATION.png";
 
 class Header extends Component {
-    render () {
-        return(
-            <div>
+    render() {
+        return (
+            <div className="header">
                 <header>
-
-            <div className="header-top">
-                <img src={NavProfile} id="profile-logo"></img>
-                <img src={NavStar} id="star-logo"></img>    
-                <img src={Logo} id="head-logo" ></img>
-                <img src={NavSearch} id="search-logo"></img>
-                <img src={NavLocation} id="location-logo"></img>
-                {/* <img src={Logo}></img>                                 */}
+                    <div className="banner">SALE 30% off Accessories</div>
+                    <div className="header-top">
+                        <div className="first-logos">
+                            <img src={NavProfile} id="profile-logo"></img>
+                            <img src={NavStar} id="star-logo"></img>
+                        </div>
+                        <img src={Logo} id="head-logo" ></img>
+                        <div className="second-logos">
+                            <img src={NavSearch} id="search-logo"></img>
+                            <img src={NavLocation} id="location-logo"></img>
+                        </div>
+                    </div>
+                </header>
+                <nav>
+                    <ul className="header-nav">
+                        <li>GET STARTED</li>
+                        <li>PANTS</li>
+                        <li>TOPS</li>
+                        <li>OUTERWEAR</li>
+                        <li>SHOES</li>
+                        <li>ACCESORIES</li>
+                        <li>SALE</li>
+                    </ul>
+                </nav>
             </div>
-        </header>
-        <nav>            
-            <ul className="header-nav">
-                <li>GET STARTED</li>
-                <li>PANTS</li>
-                <li>TOPS</li>
-                <li>OUTERWEAR</li>
-                <li>SHOES</li>
-                <li>ACCESORIES</li>
-                <li>SALE</li>
-            </ul>
-        </nav>
-        </div>
-        ) 
+        )
     }
 }
 
