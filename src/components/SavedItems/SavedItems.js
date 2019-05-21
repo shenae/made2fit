9ee0177a3d8203cb58f73185c64d2d53e081a6ef
Jack Gay <jackpowis@gmail.com>
2019-05-21 08:39:51 -0400

    changed stuff

--- src/components/SavedItems/SavedItems.js
+++ src/components/SavedItems/SavedItems.js
@@ -18,38 +18,38 @@ class SavedItems extends Component {
                 <div className="saved-grid">
                     <div>
                         <Link to="/Specific" component= { SpecificPage}><img className="img-saved" src={image1} alt="blue chinos" />
-                        </Link><p className="saved-item">WASHED CHINOS</p><div className="saved-price">$65</div>
+                        </Link><p className="saved-item">WASHED CHINOS<p className="saved-price">$65</p></p><div className="saved-description">Breezy</div>
                     </div>
                     <div>
                         <Link to="/Specific" component= { SpecificPage}><img className="img-saved" src={image2} alt="navy blue chinos" />
-                        </Link><p className="saved-item">STRETCH CHINOS</p><div className="saved-price">$45</div>
+                        </Link><p className="saved-item">STRETCH CHINOS<p className="saved-price">$45</p></p><div className="saved-description">Sky Blue</div>
                     </div>
                     <div>
                         <Link to="/Specific" component= { SpecificPage}><img className="img-saved" src={image3} alt="blue chinos" />
-                        </Link><p className="saved-item">COTTON CHINOS</p><div className="saved-price">$80</div>
+                        </Link><p className="saved-item">COTTON CHINOS<p className="saved-price">$50</p></p><div className="saved-description">Ink Blue</div>
                     </div>
                     <div>
                         <Link to="/Specific" component= { SpecificPage}><img className="img-saved" src={image4} alt="jacket" />
-                        </Link><p className="saved-item">DENIM JACKET</p><div className="saved-price">$50</div>
+                        </Link><p className="saved-item">DENIM JACKET<p className="saved-price">$80</p></p><div className="saved-description">Navy</div>
                     </div>
                 </div>
                 
                 <div className="saved-grid">
                     <div>
                         <Link to="/Specific" component= { SpecificPage}><img className="img-saved" src={image5} alt="red cashmere sweater" />
-                        </Link><p className="saved-item">FRONT ZIP SWEATER</p><div className="saved-price">$88</div>
+                        </Link><p className="saved-item">FRONT ZIP SWEATER<p className="saved-price">$88</p></p><div className="saved-description">Dark Blue</div>
                     </div>
                     <div>
                         <Link to="/Specific" component= { SpecificPage}><img className="img-saved" src={image6} alt="blue chinos" />
-                        </Link><p className="saved-item">WASHED CHINOS</p><div className="saved-price">$65</div>
+                        </Link><p className="saved-item">WASHED CHINOS<p className="saved-price">$65</p></p><div className="saved-description">Navy</div>
                     </div>
                     <div>
                         <Link to="/Specific" component= { SpecificPage}><img className="img-saved" src={image7} alt="blue chinos" />
-                        </Link><p className="saved-item">COTTON CHINOS</p><div className="saved-price">$80</div>
+                        </Link><p className="saved-item">COTTON CHINOS<p className="saved-price">$80</p></p><div className="saved-description">Dark Blue</div>
                     </div>
                     <div>
                         <Link to="/Specific" component= { SpecificPage}><img className="img-saved" src={image8} alt="navy blue blazer" />
-                        </Link><p className="saved-item">LINEN BLAZER</p><div className="saved-price">$150</div>
+                        </Link><p className="saved-item">LINEN BLAZER<p className="saved-price">$150</p></p><div className="saved-description">Navy</div>
                     </div>
                 </div>
             </div>
